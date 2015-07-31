fe204dcc59928ff70259a30ceace54d1e455d594
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 19:20:18 -0700

    Refactored using Mongo. Setup MongoLab on Azure.

--- app/models/link.js
+++ app/models/link.js
@@ -1,22 +1,6 @@
 var mongooseDb = require('../config');
 var crypto = require('crypto');
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
-
 var linkSchema = mongooseDb.Schema({
   url: String,
   base_url: String,
@@ -25,18 +9,14 @@ var linkSchema = mongooseDb.Schema({
   visits: Number
 });
 
-linkSchema.methods.createHash = function(callback) {
+linkSchema.methods.createHash = function(context) {
   var shasum = crypto.createHash('sha1');
-  shasum.update(this.url);
-  this.code = shasum.digest('hex').slice(0, 5);
-  this.save();
+  context.code = shasum.digest('hex').slice(0, 5);
+
+  console.log(context, context.code);
 };
-
-linkSchema.post('save', function(link) {
-  this.createHash();
-
-});
 
 var Link = mongooseDb.model('Link', linkSchema);
 
 module.exports = Link;
+
--- app/mongodb.js
+++ app/mongodb.js
@@ -1,35 +1,9 @@
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my_database');
+if (process.env.NODE_ENV === 'production')
+  mongoose.connect('mongodb://MongoLab-o:BtguKZEBvXF0neM_zsSfvTlR28j7mW5SKFv1WgyHTVg-@ds038888.mongolab.com:38888/MongoLab-o');
+else
+  mongoose.connect('mongodb://localhost/my_database');
 
 var db = mongoose.connection;
 
